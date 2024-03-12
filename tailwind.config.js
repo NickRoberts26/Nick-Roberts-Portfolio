a37302e81d717a9f299f6c6c03ba404e46c85a6e
nroberts16 <n26roberts@gmail.com>
2024-03-13 00:12:19 +1100

    Built simple portfolio site - still missing copy

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,19 @@ module.exports = {
       'xs': '361px',
       ...defaultTheme.screens,
     },
+    spacing: {
+      top: '100px',
+      ...defaultTheme.spacing,
+    },
     extend: {
       colors: {
-        'primary-color': '#0fa'
+        'primary-color': '#0fa',
+        'heading': '#30d4a1',
+        'body': '#94a3b8'
       }
+    },
+    fontFamily: {
+      source: ['"Source Code Pro"', ...defaultTheme.fontFamily.sans]
     },
   },
   plugins: [],
